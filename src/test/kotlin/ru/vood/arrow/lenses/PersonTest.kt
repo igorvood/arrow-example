ac67869b77ffd2ff73f477db1c87e083b46f75db
vood <igor-vood@gmail.com>
2024-09-16 20:54:59 +0300

    init

--- src/test/kotlin/ru/vood/arrow/lenses/PersonTest.kt
+++ src/test/kotlin/ru/vood/arrow/lenses/PersonTest.kt
@@ -1,5 +1,6 @@
 package ru.vood.arrow.lenses
 
+import arrow.optics.Lens
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -9,7 +10,7 @@ import ru.vood.arrow.example.ru.vood.arrow.lenses.*
 class PersonTest {
 
     @Test
-    fun example() {
+    fun Operations() {
         val me = Person(
             "Alejandro", 35,
             Address(Street("Kotlinstraat", 1), City("Hilversum", "Netherlands"))
@@ -23,5 +24,19 @@ class PersonTest {
         val newAddress = Address(Street("Kotlinplein", null), City("Amsterdam", "Netherlands"))
         val meAfterMoving = Person.address.set(me, newAddress)
         Person.address.get(meAfterMoving) shouldBe newAddress
+    }
+
+
+    val personCity =
+        Person.address compose Address.city compose City.name
+    @Test
+    fun Composition() {
+        val me = Person(
+            "Alejandro", 35,
+            Address(Street("Kotlinstraat", 1), City("Hilversum", "Netherlands"))
+        )
+
+        personCity.get(me) shouldBe "Hilversum"
+        val meAtTheCapital = personCity.set(me, "Amsterdam")
     }
 }
