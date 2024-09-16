86325adb9232fe0ff5c10c148f1fd49ce8d2bd57
vood <igor-vood@gmail.com>
2024-09-16 21:10:29 +0300

    init

--- src/test/kotlin/ru/vood/arrow/reflection/PersonTest.kt
+++ src/test/kotlin/ru/vood/arrow/reflection/PersonTest.kt
@@ -1,5 +1,7 @@
 package ru.vood.arrow.reflection
 
+import arrow.optics.Every
+import arrow.optics.instance
 import arrow.optics.lens
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
@@ -14,5 +16,18 @@ class PersonTest {
         val p = Person("me", listOf("pat", "mat"))
         val m = Person::name.lens.modify(p) { it.capitalize() }
         m.name shouldBe "Me"
+    }
+
+
+    sealed interface Cutlery
+    object Fork: Cutlery
+    object Spoon: Cutlery
+
+    @Test
+    fun Prisms() {
+        val things = listOf(Fork, Spoon, Fork)
+        val forks = Every.list<Cutlery>() compose instance<Cutlery, Fork>()
+        val noOfForks = forks.size(things)
+        noOfForks shouldBe 2
     }
 }
