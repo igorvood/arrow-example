f109e5119653fea8c4343adb71f1e69f6cac3a35
vood <igor-vood@gmail.com>
2024-10-05 13:33:42 +0300

    init

--- src/main/kotlin/ru/vood/arrow/context/TrickyContext.kt
+++ src/main/kotlin/ru/vood/arrow/context/TrickyContext.kt
@@ -3,33 +3,33 @@ package ru.vood.arrow.example.ru.vood.arrow.context
 import java.util.*
 
 context(String, Int)
-fun s(): Unit{
+fun s(): Unit {
     println("------ String, Int context")
     println(toLong())
     println(substring(1))
 }
 
 context(String)
-fun s(): Unit{
+fun s(): Unit {
     println("------ String context")
     println(uppercase(Locale.getDefault()))
 }
 
 context(Int)
-fun s(): Unit{
+fun s(): Unit {
     println("------ Int context")
-    println(toLong()+1)
+    println(toLong() + 1)
 }
-fun main(){
 
+fun main() {
     "1qwerty".apply {
         s()
-        15.apply {
+        with(15) {
             s()
         }
     }
 
-    23.apply{
+    23.apply {
         s()
     }
 
