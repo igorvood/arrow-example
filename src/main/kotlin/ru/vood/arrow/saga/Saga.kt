af694d4cf6837c75a26b0c9f7543bd1cbf0104f6
vood <igor-vood@gmail.com>
2024-10-05 12:17:59 +0300

    init

--- src/main/kotlin/ru/vood/arrow/saga/Saga.kt
+++ src/main/kotlin/ru/vood/arrow/saga/Saga.kt
@@ -9,30 +9,30 @@ import kotlinx.coroutines.runBlocking
 import ru.vood.arrow.example.ru.vood.arrow.errorHandling.*
 
 
-//context(Raise<UserExists>, SagaScope)
-//suspend fun insertOrRollBack(userName: String): User {
-//    val saga = saga({ insertUser(userName) }) { user -> println("удаляю пользователя")
-//    User}
-//    return saga
-//}
-//
-//
-//context(Raise<UserRegistrationError>)
-//suspend fun route2(request: HttpRequest): HttpResponse {
-//    val name = request.userName()
-//    saga {
-//
-//        val user = insertOrRollBack(name)
-//        user.receivePayment()
-//    }.transact()
-//    return HttpResponse.CREATED
-//
-//}
-//
-//fun main() {
-//    runBlocking {
-//        recover({ route2(HttpRequest) }) {
-//            println("цепочка создание пользователя, регисстрация его и проведение платежа по причине ${it.javaClass.name}")
-//        }
-//    }
-//}
+context(Raise<UserExists>)
+suspend fun SagaScope.insertOrRollBack(userName: String): User {
+    val saga = saga({ insertUser(userName) }) { user -> println("удаляю пользователя")
+    User}
+    return saga
+}
+
+
+context(Raise<UserRegistrationError>)
+suspend fun route2(request: HttpRequest): HttpResponse {
+    val name = request.userName()
+    saga {
+
+        val user = insertOrRollBack(name)
+        user.receivePayment()
+    }.transact()
+    return HttpResponse.CREATED
+
+}
+
+fun main() {
+    runBlocking {
+        recover({ route2(HttpRequest) }) {
+            println("цепочка создание пользователя, регисстрация его и проведение платежа по причине ${it.javaClass.name}")
+        }
+    }
+}
